95150fa2f9f334e6e805669446bbf34dee4dffda
HackerXXXX007 <1748398874wch@gmail.com>
2024-04-26 17:13:42 +0800

    4_26

---
+++ lab/lab03/lab03.py
@@ -0,0 +1,238 @@
+from operator import add, mul
+from math import sqrt
+
+square = lambda x: x * x
+
+identity = lambda x: x
+
+triple = lambda x: 3 * x
+
+increment = lambda x: x + 1
+
+
+def ordered_digits(x):
+    """Return True if the (base 10) digits of X>0 are in non-decreasing
+    order, and False otherwise.
+
+    >>> ordered_digits(5)
+    True
+    >>> ordered_digits(11)
+    True
+    >>> ordered_digits(127)
+    True
+    >>> ordered_digits(1357)
+    True
+    >>> ordered_digits(21)
+    False
+    >>> result = ordered_digits(1375) # Return, don't print
+    >>> result
+    False
+
+    """
+    "*** YOUR CODE HERE ***"
+    if x<10:                           #if x<10 return True
+        return True
+    
+    digits=[]                          #Find each position of the number and place it in the array
+    while x>0:
+        digit=x%10
+        digits.append(digit)
+        x=x//10
+    digits.reverse()
+
+    for i in range(len(digits)-1):     #if digits of x>=10 in non-decreasing order, and False otherwise
+        if digits[i]>digits[i+1]:
+            return False
+    return True
+
+
+def get_k_run_starter(n, k):
+    """Returns the 0th digit of the kth increasing run within n.
+    >>> get_k_run_starter(123444345, 0) # example from description      1234  4  4  345
+    3
+    >>> get_k_run_starter(123444345, 1)           
+    4
+    >>> get_k_run_starter(123444345, 2)
+    4
+    >>> get_k_run_starter(123444345, 3)
+    1
+    >>> get_k_run_starter(123412341234, 1)
+    1
+    >>> get_k_run_starter(1234234534564567, 0)
+    4
+    >>> get_k_run_starter(1234234534564567, 1)
+    3
+    >>> get_k_run_starter(1234234534564567, 2)
+    2
+    """
+    i = 0
+    final = None
+    while i<=k:                                                    #Points to the K+1th sequence
+        index=len(str(n))-1                                        #Use an index to point to the last number of number n
+        while index>0 and str(n)[index-1]<str(n)[index]:
+            index=index-1
+        final = int(str(n)[index])
+        i = i+1
+        n = str(n)[:index]                                         #Cut off the pointed sequence in reverse order
+    return final
+
+
+def make_repeater(func, n):
+    """Return the function that computes the nth application of func.
+
+    >>> add_three = make_repeater(increment, 3)
+    >>> add_three(5)
+    8
+    >>> make_repeater(triple, 5)(1) # 3 * 3 * 3 * 3 * 3 * 1
+    243
+    >>> make_repeater(square, 2)(5) # square(square(5))
+    625
+    >>> make_repeater(square, 4)(5) # square(square(square(square(5))))
+    152587890625
+    >>> make_repeater(square, 0)(5) # Yes, it makes sense to apply the function zero times!
+    5
+    """
+    "*** YOUR CODE HERE ***"
+    def repeate_func(x):
+        result=x
+        if n==0:
+            return result
+        else:
+            for _ in range(n):
+                result=func(result)
+            return result
+    return repeate_func
+
+
+def composer(func1, func2):
+    """Return a function f, such that f(x) = func1(func2(x))."""
+    def f(x):
+        return func1(func2(x))
+    return f
+
+
+def apply_twice(func):
+    """ Return a function that applies func twice.
+
+    func -- a function that takes one argument
+
+    >>> apply_twice(square)(2)
+    16
+    """
+    "*** YOUR CODE HERE ***"
+    def twice_function(x):
+        return func(func(x))
+    return twice_function
+
+
+def div_by_primes_under(n):
+    """
+    >>> div_by_primes_under(10)(11)
+    False
+    >>> div_by_primes_under(10)(121)
+    False
+    >>> div_by_primes_under(10)(12)
+    True
+    >>> div_by_primes_under(5)(1)
+    False
+    """
+    checker = lambda x: False
+    i =2
+    while i<=n:
+        if not checker(i):
+            checker = ____________________________
+        i = i+1
+    return checker
+
+
+def div_by_primes_under_no_lambda(n):
+    """
+    >>> div_by_primes_under_no_lambda(10)(11)
+    False
+    >>> div_by_primes_under_no_lambda(10)(121)
+    False
+    >>> div_by_primes_under_no_lambda(10)(12)
+    True
+    >>> div_by_primes_under_no_lambda(5)(1)
+    False
+    """
+    def checker(x):
+        return False
+    i = ____________________________
+    while ____________________________:
+        if not checker(i):
+            def outer(____________________________):
+                def inner(____________________________):
+                    return ____________________________
+                return ____________________________
+            checker = ____________________________
+        i = ____________________________
+    return ____________________________
+
+
+def zero(f):
+    return lambda x: x
+
+
+def successor(n):
+    return lambda f: lambda x: f(n(f)(x))
+
+
+def one(f):
+    """Church numeral 1: same as successor(zero)"""
+    "*** YOUR CODE HERE ***"
+
+
+def two(f):
+    """Church numeral 2: same as successor(successor(zero))"""
+    "*** YOUR CODE HERE ***"
+
+
+three = successor(two)
+
+
+def church_to_int(n):
+    """Convert the Church numeral n to a Python integer.
+
+    >>> church_to_int(zero)
+    0
+    >>> church_to_int(one)
+    1
+    >>> church_to_int(two)
+    2
+    >>> church_to_int(three)
+    3
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def add_church(m, n):
+    """Return the Church numeral for m + n, for Church numerals m and n.
+
+    >>> church_to_int(add_church(two, three))
+    5
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def mul_church(m, n):
+    """Return the Church numeral for m * n, for Church numerals m and n.
+
+    >>> four = successor(three)
+    >>> church_to_int(mul_church(two, three))
+    6
+    >>> church_to_int(mul_church(three, four))
+    12
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def pow_church(m, n):
+    """Return the Church numeral m ** n, for Church numerals m and n.
+
+    >>> church_to_int(pow_church(two, three))
+    8
+    >>> church_to_int(pow_church(three, two))
+    9
+    """
+    "*** YOUR CODE HERE ***"
