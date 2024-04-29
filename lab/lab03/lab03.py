399550c0a2ef72660a4b494871a43c956badd378
HackerXXXX007 <1748398874wch@gmail.com>
2024-04-29 21:17:04 +0800

    Recursion

--- lab/lab03/lab03.py
+++ lab/lab03/lab03.py
@@ -181,11 +181,13 @@ def successor(n):
 def one(f):
     """Church numeral 1: same as successor(zero)"""
     "*** YOUR CODE HERE ***"
+    return lambda x: f(x)
 
 
 def two(f):
     """Church numeral 2: same as successor(successor(zero))"""
     "*** YOUR CODE HERE ***"
+    return lambda x: f(f(x))
 
 
 three = successor(two)
@@ -204,6 +206,7 @@ def church_to_int(n):
     3
     """
     "*** YOUR CODE HERE ***"
+    return n(lambda x:x+1)(0)
 
 
 def add_church(m, n):
@@ -213,6 +216,7 @@ def add_church(m, n):
     5
     """
     "*** YOUR CODE HERE ***"
+    return lambda f:lambda x:m(f)(n(f)(x))
 
 
 def mul_church(m, n):
